1f59d17051d6e378394213d8f19f1ced5b580947
vcky4 <okon.victor.u@gmail.com>
2025-11-23 20:16:11 +0100

    chore: update licensing

--- apps/backend/src/main.py
+++ apps/backend/src/main.py
@@ -1,3 +1,11 @@
+# Maigie - AI-powered student companion
+# Copyright (C) 2025 Victor Okon
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 """FastAPI application entry point."""
 
 from fastapi import FastAPI
